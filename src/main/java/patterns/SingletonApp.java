7e0c696db329f5349dffd5c9a0dfb63ce72195c7
Denis Yevduschenko <a2ka7.dp@gmail.com>
2016-04-07 00:51:22 +0300

    Singleton

---
+++ src/main/java/patterns/SingletonApp.java
@@ -0,0 +1,34 @@
+package patterns;
+
+public class SingletonApp {
+	public static void main(String[] args){
+		//Error: doesn't exist visible constructor
+		//TestSingleton ts = new TestSingleton();
+		
+		TestSingleton.getInstance().print();
+		
+		TestSingleton.getInstance().stop();
+	}
+}
+
+class TestSingleton{
+	public static TestSingleton instance;
+	
+	public static synchronized TestSingleton getInstance(){
+		if (instance == null){
+			instance = new TestSingleton();
+		}
+		return instance;
+	}
+	
+	private TestSingleton(){	
+	}
+	
+	public void print(){
+		System.out.println(this);
+	}
+	
+	public void stop(){
+		System.out.println("Stop!");
+	}
+}
