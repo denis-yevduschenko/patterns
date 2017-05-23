9e80231ec5b48430eee2d1ddc3c849d08a1ac536
dyevduschenko <dyevduschenko>
2017-05-23 09:03:02 +0300

    JS Singleton

---
+++ js_patterns/singleton.js
@@ -0,0 +1,19 @@
+var Universe;
+	(function () {
+		var instance;
+		Universe = function Universe() {
+		if (instance) {
+			return instance;
+		}
+		instance = this;
+		// добавить остальную функциональность
+		this.start_time = 0;
+		this.bang = "Big";
+	};
+}());
+// проверка
+var uni = new Universe();
+var uni2 = new Universe();
+console.log(uni === uni2); // true
+console.log(uni.constructor === Universe); // true
+console.log(uni.bang);
